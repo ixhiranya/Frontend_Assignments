363fae8e48171fefeb5f3a252df2e3350070effc
ixhiranya <madupuhiranya07@gmail.com>
2026-02-17 11:43:56 +0530

    added local storage

--- FigmaTemplate/script.js
+++ FigmaTemplate/script.js
@@ -2,6 +2,7 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const email = document.getElementById("email").value.trim();
   const pass = document.getElementById("password").value.trim();
+  const remember = document.querySelector('input[type="checkbox"]').checked;
   if (email === "" || pass === "") {
     alert("Please enter email and password");
     return;
@@ -14,5 +15,23 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     alert("Password too short");
     return;
   }
-  alert("Login successful");
+  let users=JSON.parse(localStorage.getItem("sv_users")) || [];
+  const exisitingUser=users.find(u=>u.email===email);
+  if(exisitingUser){
+    if(exisitingUser.password===pass){
+      alert("Login Successful!!");
+      if(remember){
+        localStorage.setItem("sv_current_user",email);
+      }
+    }else{
+      alert("Wrong Password");
+    }
+  }else{
+    users.push({email:email,password:pass});
+    localStorage.setItem("sv_users",JSON.stringify(users));
+    alert("New user registered and logged in..!!");
+    if(remember){
+      localStorage.setItem("sv_current_user",email);
+    }
+  }
 });
